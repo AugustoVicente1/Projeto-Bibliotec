e79dec2f992773c63449671747a635a05961ce63
AUGUSTO VICENTE SANTOS <23977290835@info.local>
2026-09-10 11:02:34 -0300

    novo commit

--- login/login.js
+++ login/login.js
@@ -1,27 +1,36 @@
-// console.log ("hello word")
+
+
+// console.log("Hello world!!");
 
 // alert("Hello world!!");
 
 // var nome = "Késsia" !não vamos usar o var
 
-// let nome = "Kessia"
+// let nome = "Késsia"
+// console.log(nome)
 
+// nome = "Pedro"
+// console.log(nome)
+
+// const nome = "Késsia"
+// console.log(nome)
+
+// nome = "Pedro"
+// console.log(nome)
 
 const campoSenha = document.getElementById("senha");
 const btnSenha = document.querySelector("#mostrar-senha");
 
-console.log](btnSenha.type);
 
 btnSenha.addEventListener("click", function() {
-
-    // if(campoSenha.type == "password"){
-    //     campoSenha.type ="text";
-    // }
-    // else{
-    //     campoSenha.type = "password"; 
-    // }
     
-    campoSenha.type = campoSenha.type == "password" ? "text" : "password"
-     
+    // if(campoSenha.type == "password") {
+    //     campoSenha.type = "text";
+    // } 
+    // else {
+    //     campoSenha.type = "password";
+    // }
 
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password"
+    
 })
